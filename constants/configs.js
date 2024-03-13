3c261bbda046bfb480761f8e3af6b49a4b0f581c
JorgeDircio <jorgeluispedro16@gmail.com>
2024-03-13 16:59:55 -0600

    mejora en conexion a mongo

--- constants/configs.js
+++ constants/configs.js
@@ -3,11 +3,14 @@ const API_VERSION =  process.env.API_VERSION;
 const IP_SERVER = process.env.IP_SERVER;
 const HOST_DB = process.env.HOST_BD;
 const BD_NAME = process.env.BD_NAME;
-
+const USER_NAME_BD = process.env.USER_NAME_BD;
+const PASSWORD_USER_BD = process.env.PASSWORD_USER_BD;
 module.exports = {
     PORT,
     API_VERSION,
     IP_SERVER,
     HOST_DB,
-    BD_NAME
+    BD_NAME,
+    USER_NAME_BD,
+    PASSWORD_USER_BD
 }
--- databases/configs.js
+++ databases/configs.js
@@ -1,16 +1,21 @@
 const mongoose = require('mongoose');
-const { HOST_DB, BD_NAME } = require('../constants/configs');
+const { HOST_DB, BD_NAME, USER_NAME_BD, PASSWORD_USER_BD } = require('../constants/configs');
 const uri = `${HOST_DB}${BD_NAME}`;
 
 const dbConnection = async () => {
     try {
-      mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-      console.log('DATABASE connection established');
+        await mongoose.connect(uri, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            user: USER_NAME_BD,
+            pass: PASSWORD_USER_BD,
+        });
+        console.log('DATABASE connection established');
     } catch (error) {
-      console.log(error);
-      throw new Error('Error in the connection');
+        console.error(error);
+        throw new Error('Error in the connection');
     }
-  };
+};
 
 module.exports = {
     dbConnection
